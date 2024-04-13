0f1d5a1c7381f2e0bb9a2ece6368671e50d6d5a2
zozibush <zozibush88@gmail.com>
2024-04-14 03:33:20 +0900

    Fix uvicorn.run() call in main.py to use the application object

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(user_router, prefix="/user")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1" , port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1" , port=8000, reload=True)
