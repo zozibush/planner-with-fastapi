90a76709045d1a7c910cd6698cfd55a0d71ca8c7
zozibush <zozibush88@gmail.com>
2024-04-14 17:47:11 +0900

    Add retrieve_event endpoint to events API router

--- routes/events.py
+++ routes/events.py
@@ -11,3 +11,13 @@ events = []
 @event_router.get("/", response_model=List[Event])
 async def retrieve_events() -> List[Event]:
     return events
+
+@event_router.get("/{id}", response_model=Event)
+async def retrieve_event(id: int) -> Event:
+    for event in events:
+        if event.id == id:
+            return event
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail = "Event with supplied ID does not exist"
+    )
