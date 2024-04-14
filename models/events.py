4dc91ba5093545aecc4591b1639183ae9c0d7fc1
zozibush <zozibush88@gmail.com>
2024-04-14 20:11:22 +0900

    Update Event model in events.py to include optional fields for updating events

--- models/events.py
+++ models/events.py
@@ -20,3 +20,21 @@ class Event(SQLModel, table=True):
                 "location": "Google Meet"
             }
         }
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tag: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event.Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
