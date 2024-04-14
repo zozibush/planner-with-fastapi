47f6ddbb3f33a9c9b8a5c71558ea43c4d7d0b33e
zozibush <zozibush88@gmail.com>
2024-04-14 20:09:25 +0900

    Update Event model in events.py to use SQLModel and add tags field

--- models/events.py
+++ models/events.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import List, Optional
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
 
     class Config:
+        arbitrary_types_allowed = True
         json_schema_extra = {
             "example": {
                 "title": "FastAPI Book Launch",
