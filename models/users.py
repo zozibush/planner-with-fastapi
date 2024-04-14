7cd205fa5e1245921cb5e0281bd33d09032e59d7
zozibush <zozibush88@gmail.com>
2024-04-14 21:55:31 +0900

    Update User model in users.py to use str instead of EmailStr for email field

--- models/users.py
+++ models/users.py
@@ -1,31 +1,28 @@
 from sqlmodel import JSON, SQLModel, Field, Column
-from pydantic import EmailStr
 from typing import Optional, List
 from models.events import Event
 
 class User(SQLModel, table=True):
-    email: EmailStr = Field(default=None, primary_key=True)
+    email: str = Field(default=None, primary_key=True)
     password: str
-    events: Optional[List[Event]] = Field(sa_column=Column(JSON))
 
     class Config:
+        arbitrary_types_allowed = True
         json_schema_extra = {
             "example": {
                 "email": "fastapi@packt.com",
                 "password": "strong!!!",
-                "events": [],
             }
         }
 
 class UserSignIn(SQLModel):
-    email: EmailStr
+    email: str
     password: str
 
     class Config:
         json_schema_extra = {
             "example": {
                 "email": "fastapi@packt.com",
-                "password": "strong!!!",
-                "events": [],
+                "password": "strong!!!"
             }
         }
