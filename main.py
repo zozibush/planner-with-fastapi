d98aaab103088afe614c8595ec83eb04a3fd7a58
zozibush <zozibush88@gmail.com>
2024-04-14 20:13:51 +0900

    Add database connection module and import it in main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn
 
 import uvicorn
 
 app = FastAPI()
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+@app.on_event("startup")
+def on_startup():
+    conn()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1" , port=8000, reload=True)
