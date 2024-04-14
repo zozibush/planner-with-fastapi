38b1dfc7eff338d8c8ddc2889806b78c253d5a49
zozibush <zozibush88@gmail.com>
2024-04-14 20:23:48 +0900

    Add update_event endpoint in events API router to use database session

--- routes/events.py
+++ routes/events.py
@@ -37,6 +37,24 @@ async def create_event(new_event: Event, session=Depends(get_session)) -> dict:
         "message":"Event created successfully."
     }
 
+@event_router.put("/edit/{id}", response_model=Event)
+async def update_event(id: int, new_data: EventUpdate, session=Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.dict(exclude_unset=True)
+        for key, value in event_data.items():
+            setattr(event, key, value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+
+        return event
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail = "Event with supplied ID does not exist"
+    )
+
 @event_router.delete("/{id}")
 async def delete_event(id: int) -> dict:
     for event in events:
